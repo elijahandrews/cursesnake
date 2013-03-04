e328442d07553dbd4ec761250a77f2bdd63ebd47
Elijah Andrews <elijahcandrews@gmail.com>
2013-03-04 01:28:53 -0500

    Add velocity attribute and move method to Character Update main loop to use non-blocking input to populate velocity

--- character.rb
+++ character.rb
@@ -1,19 +1,26 @@
 require './entity.rb'
 
 class Character < Entity
+  attr_accessor :velocity # [y_offset, x_offset]
 
-  def initialize(y, x, char)
+  def initialize(y, x, char, velocity)
     @y = y
     @x = x
     @char = char
+    @velocity = velocity
     draw
   end
 
-   def move(y, x)
-    Display.draw @y, @x, ' '
-    @y += y
-    @x += x
-    draw
+  def move_by_velocity
+    # Consider refactoring Display to be in instance of an object instead of a static class
+    y_offset = velocity.first
+    x_offset = velocity.last
+    if Display.instance_variable_get(:@tiles)[@y+y_offset][@x+x_offset].traversable
+      Display.draw @y, @x, ' '
+      @y += y_offset
+      @x += x_offset
+      draw
+    end
   end
 
 end
--- cursesnake.rb
+++ cursesnake.rb
@@ -2,19 +2,40 @@ require './display.rb'
 require './tile.rb'
 require './entity.rb'
 require './character.rb'
+require 'io/wait'
+
+def char_if_pressed
+  begin
+    system("stty raw -echo") # turn raw input on
+    c = nil
+    if $stdin.ready?
+      c = $stdin.getc
+    end
+    c.chr if c
+  ensure
+    system "stty -raw echo" # turn raw input off
+  end
+end
 
 Display::init_screen do
+  Curses.timeout = 0
   Display::initiate_tiles
+  char = Character.new 20, 20, '@', [0,0]
   Curses.refresh
-  char = Character.new 20, 20, '@'
   loop do
-    case Curses.getch
-    when Curses::Key::UP then char.move(-1,0)
-    when Curses::Key::DOWN then char.move(1,0)
-    when Curses::Key::RIGHT then char.move(0,1)
-    when Curses::Key::LEFT then char.move(0,-1)
-    when (?q) then break
+    c = Curses.getch
+    unless c.nil?
+      case c
+      when Curses::Key::UP    then char.velocity = [-1, 0]
+      when Curses::Key::DOWN  then char.velocity = [1, 0]
+      when Curses::Key::RIGHT then char.velocity = [0, 1]
+      when Curses::Key::LEFT  then char.velocity = [0, -1]
+      when (?q) then break
+      end
     end
+    char.move_by_velocity
+    Curses.refresh
+    sleep(0.1)
   end
 end
 
